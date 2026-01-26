793672de56f15f2072aab5fe4ed2c580b8744388
dsalgado9000 <d.salgado9000@gmail.com>
2026-01-26 00:49:02 -0600

    Add files via upload

---
+++ PS1P3.cpp
@@ -0,0 +1,20 @@
+//////
+//////  PS3P1.cpp
+//////  PS1P1
+//////
+//////  Created by Daniel Salgado on 1/25/26.
+//////
+////
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+    std:: string lastname;
+    double number1;
+    std:: cout << "Please enter your last name and score result";
+    std:: cin >> lastname >> number1;
+    std:: cout << lastname << " has a score of " << number1 << std::endl;
+    
+    return 0;
+}
+
